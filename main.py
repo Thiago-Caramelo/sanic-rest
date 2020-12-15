55a127ba734e4f01bc5b368abc6d491d10864d9a
Thiago Caramelo <thiago.pescado@gmail.com>
2020-12-15 11:41:30 -0300

    Add update method

--- main.py
+++ main.py
@@ -9,6 +9,21 @@ import db
 app = Sanic("app")
 
 
+@app.route('/person/<person_id>', methods=['PUT'])
+async def post_handler(request, person_id):
+    async with db.pool.acquire() as conn:
+        sql = '''
+                UPDATE public.person
+                SET name=COALESCE($1,name), guid=COALESCE($2,guid), dt=COALESCE($3,dt)
+                WHERE id = $4
+                '''
+        dt_value = request.json.get("dt", None)
+        dt = None if dt_value == None else datetime.datetime.fromisoformat(
+            dt_value)
+        rows = await conn.execute(sql, request.json.get("name", None), request.json.get("guid", None), dt, int(person_id))
+        return response.json(request.json, status=200)
+
+
 @app.post("/person")
 async def create_person(request):
     async with db.pool.acquire() as conn:
@@ -21,25 +36,20 @@ async def create_person(request):
         return response.json({}, status=201)
 
 
-@app.route("/wait")
-async def wait(request):
-    async with db.pool.acquire() as conn:
-        sql = '''
-                    SELECT pg_sleep(1); 
-                '''
-        rows = await conn.fetchrow(sql)
-        return response.json(json_utils.converter(rows), status=200)
-
-
 @app.route("/person")
 async def person(request):
+    params = dict(request.query_args)
+    name = params.get('name', None)
+    if name != None:
+        name = '%{}%'.format(name)
     async with db.pool.acquire() as conn:
         sql = '''
                     SELECT * FROM public.person
+                    WHERE ( $1::varchar IS NULL OR name ILIKE $1::varchar )
                     ORDER BY id desc
                     LIMIT 10;
                 '''
-        rows = await conn.fetch(sql)
+        rows = await conn.fetch(sql, name)
         return response.json(json_utils.converter(rows), status=200)
 
 
